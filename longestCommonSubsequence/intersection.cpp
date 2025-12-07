31c62ca40ea44baf319696bb3b6405506e12496a
GagTad <136594120+GagTad@users.noreply.github.com>
2025-12-07 20:50:51 +0400

    Update intersection.cpp

--- longestCommonSubsequence/intersection.cpp
+++ longestCommonSubsequence/intersection.cpp
@@ -1,7 +1,7 @@
 //LeetCode349
 //Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
 
-vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+std::vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
        std::unordered_set<int> s1(nums1.begin(), nums1.end());
        std::unordered_set<int> resSet;
         for (int num : nums2) {
@@ -18,12 +18,12 @@ vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
 //Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
 
 
-    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int, int> counts;
+    std::vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        std::unordered_map<int, int> counts;
         for (int num : nums1) {
         counts[num]++;
     }
-    vector<int> result;
+    std::vector<int> result;
      for (int num : nums2) {
             if (counts[num] > 0) {
                 result.push_back(num);
