43c5e1d189fb8cbd7fc9c67caf299062f5de10d0
GagTad <136594120+GagTad@users.noreply.github.com>
2025-12-14 22:39:49 +0400

    Update Polynomial.cpp

--- Polynomial/Polynomial.cpp
+++ Polynomial/Polynomial.cpp
@@ -12,52 +12,41 @@ void SolveAndPrintPolynomial(const std::vector<pair<int, int>>& inputTerms) {
     }
 
     std::cout << " Polynomial: ";
-
     bool isFirst = true;
     bool isEmpty = true; 
 
     for (const auto& [deg, coef] : polyMap) {
         if (coef == 0) continue;
-
         isEmpty = false;
-
         if (!isFirst) {
             if (coef > 0) std::cout << " + ";
             else cout << " - ";
         } else {
             if (coef < 0) std::cout << "-";
         }
-        
         int absCoef = abs(coef);
-
         if (deg == 0 || absCoef != 1) {
             std::cout << absCoef;
         }
-
         if (deg > 0) {
             cout << "x";
             if (deg > 1) {
                 std::cout << "^" << deg;
             }
         }
-
         isFirst = false;
     }
-
     if (isEmpty) {
         std::cout << "0";
     }
-
     std::cout << std::endl;
 }
 
 int main() {
-
     std::vector<pair<int, int>> terms = {
         {2, 3}, {4, 5}, {8, 2}, {4, -1}, {0, 7}, {0, -5}, {1, -1}
     };
-
     SolveAndPrintPolynomial(terms);
-
+    
     return 0;
 }
