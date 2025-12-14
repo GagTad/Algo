e81716890501c69d3d189ab7eebe28aa6ac70809
GagTad <136594120+GagTad@users.noreply.github.com>
2025-12-14 22:15:45 +0400

    Update hanoi.cpp

--- HanoiTowerProblem/hanoi.cpp
+++ HanoiTowerProblem/hanoi.cpp
@@ -4,8 +4,7 @@
 #include <string>
 #include <algorithm> 
 
-void printRods(const std::stack<int>& source, const std::stack<int>& final, const std::stack<int>& helper) {
-   
+void printRods(const std::stack<int>& source, const std::stack<int>& final, const std::stack<int>& helper) {  
     auto printSingleStack = [](std::string name, std::stack<int> s) {
         std::cout << name << ": [ ";
         std::vector<int> temp;
@@ -26,18 +25,14 @@ void printRods(const std::stack<int>& source, const std::stack<int>& final, cons
     std::cout << "------------------------------------" << std::endl;
 }
 
-void solveHanoi(int n,
-                std::stack<int>& source, std::stack<int>& final, std::stack<int>& helper) {
+void solveHanoi(int n,std::stack<int>& source, std::stack<int>& final, std::stack<int>& helper) {
     if (n == 0) {
         return;
     }
-
     solveHanoi(n - 1, source, helper, final);
-
     int disk = source.top();
     source.pop();
     final.push(disk);
-
     solveHanoi(n - 1, helper, final, source);
 }
 
@@ -46,18 +41,15 @@ int main() {
     std::stack<int> stick_A;
     std::stack<int> stick_B;
     std::stack<int> stick_C;
-
     int num_disks;
     std::cin >> num_disks;
-
+   
     for (int i = num_disks; i >= 1; --i) {
         stick_A.push(i);
     }
+   
     printRods(stick_A, stick_B, stick_C);
-
     solveHanoi(num_disks, stick_A, stick_C, stick_B);
-
     printRods(stick_A, stick_B, stick_C);
-
     return 0; 
 }
